2760addbf431233792675fe1d35d94baeed3049c
mikennaji <mnnaji3@uwo.ca>
2022-03-26 21:57:06 -0400

    commit tree 3ec7a0bd304544b575069074a240e7cb29214ff0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(array,string){
+    //const normalizedArray = array.map(element => element.toLowerCase());
+    //const searchString = string.toLowerCase()
+    //const results = normalizedArray.filter(element => element === searchString);
+    //const normalizedResults = results.map(element => 
+        //element[0].toUpperCase() + element.slice(1,element.length));
+    ///return normalizedResults;
+    const results = array.filter(element => {
+       if( element[0].toLowerCase()+element.slice(1,element.length)===string ||element=== string ){
+            return console.log(string)
+        }
+    })
+        
+    return results;
+}
+
+function fuzzyMatch(array,string){
+    const results = array.filter(element => element.slice(0,3).includes(string))
+    return results 
+}
+
+function matchName(driver,string){
+    const results = driver.filter(element => element.name === string )
+    return results;
+}
